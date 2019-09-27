7f58e2ad65738d2d78681034cd528c4bab91b46f
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2019-09-27 15:58:45 -0400

    Fix transposition issue

--- koppen_io.fsx
+++ koppen_io.fsx
@@ -17,13 +17,13 @@ let jmax = 180
 
 let readKoppen imax jmax = 
 
-    let rec readKoppenCol j imax jmax = 
+    let rec readKoppenCol i imax jmax =
         let rec readKoppenRow i j jmax =
             let fname = String.concat "" ["/local/home/landproj/CMIP6_historical/tmp/"; string i; "_"; string j; ".csv"]
             if not(File.Exists fname) then
-                if i = imax then [None] else None :: (readKoppenRow (i+1) j jmax)
+                if j = jmax then [None] else None :: (readKoppenRow i (j+1) jmax)
             else
-                let file = KoppenFile.Load("/local/home/landproj/CMIP6_historical/tmp/100_135.csv")
+                let file = KoppenFile.Load(fname)
                     
                 let daysInFebruary = (file.Rows |> Seq.head).DaysInFebruary * 1.0<day>
                 let hemisphere = (file.Rows |> Seq.head).Hemisphere |> function | "Northern" -> Koppen.Northern | "Southern" -> Koppen.Southern
@@ -31,14 +31,14 @@ let readKoppen imax jmax =
                 let precipitation = file.Rows |> Seq.map (fun x -> x.Precipitation) |> Seq.map (float >> (*) 1.0<mm>) |> Seq.toArray
 
                 Koppen.Climate (daysInFebruary, hemisphere, temperature, precipitation)
-                |> Koppen.Zones 
+                |> Koppen.Zones
                 |> Option.get
-                |> fun x -> if i = imax then [Some x] else Some x :: (readKoppenRow (i+1) j jmax)
-        let row = readKoppenRow 1 j jmax
-        if j = jmax then [row] else row :: (readKoppenCol (j+1) imax jmax)
+                |> fun x -> if j = jmax then [Some x] else Some x :: (readKoppenRow i (j+1) jmax)
+        let row = readKoppenRow i 1 jmax
+        if i = imax then [row] else row :: (readKoppenCol (i+1) imax jmax)
 
     readKoppenCol 1 imax jmax
     |> List.map (fun x -> x |> List.map (fun (y: Koppen.Zone Option) -> if y.IsNone then "NA\t" else y |> (Option.get >> string >> fun z -> String.concat "" [z; "\t"])))
     |> List.map (fun x -> x |> List.fold (fun acc elem -> String.concat "" [acc; elem]) "" )
-    |> fun x -> File.WriteAllLines (@"koppen.out", x)
+    |> fun x -> File.WriteAllLines (@"/local/home/landproj/CMIP6_historical/koppen.out", x)
     
