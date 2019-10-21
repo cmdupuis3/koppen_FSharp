01af186d36ecb4d6f0945f34cd97b77934cccfac
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2019-10-21 18:10:18 -0400

    Update koppen_io.fsx

--- koppen_io.fsx
+++ koppen_io.fsx
@@ -7,15 +7,15 @@ open System.IO
 open FSharp.Data
 
 
-type KoppenFile = CsvProvider<"/local/home/landproj/CMIP6_historical/tmp/100_135.csv", 
-                              HasHeaders = true, 
+type KoppenFile = CsvProvider<"/local/home/landproj/CMIP6_historical/tmp/100_135.csv",
+                              HasHeaders = true,
                               Schema = "float, string, float, float">
 let file = KoppenFile.Load("/local/home/landproj/CMIP6_historical/tmp/100_135.csv")
 
 let imax = 360
 let jmax = 180
 
-let readKoppen imax jmax = 
+let readKoppen imax jmax =
 
     let rec readKoppenCol i imax jmax =
         let rec readKoppenRow i j jmax =
@@ -24,15 +24,14 @@ let readKoppen imax jmax =
                 if j = jmax then [None] else None :: (readKoppenRow i (j+1) jmax)
             else
                 let file = KoppenFile.Load(fname)
-                    
+
                 let daysInFebruary = (file.Rows |> Seq.head).DaysInFebruary * 1.0<day>
-                let hemisphere = (file.Rows |> Seq.head).Hemisphere |> function | "Northern" -> Koppen.Northern | "Southern" -> Koppen.Southern
+                let hemisphere = (file.Rows |> Seq.head).Hemisphere |> function | "Northern" -> Koppen.Northern | "Southern" -> Koppen.Southern | _ -> failwith "fail"
                 let temperature =   file.Rows |> Seq.map (fun x -> x.Temperature)   |> Seq.map (float >> (*) 1.0<C>) |> Seq.toArray
                 let precipitation = file.Rows |> Seq.map (fun x -> x.Precipitation) |> Seq.map (float >> (*) 1.0<mm>) |> Seq.toArray
 
                 Koppen.Climate (daysInFebruary, hemisphere, temperature, precipitation)
                 |> Koppen.Zones
-                |> Option.get
                 |> fun x -> if j = jmax then [Some x] else Some x :: (readKoppenRow i (j+1) jmax)
         let row = readKoppenRow i 1 jmax
         if i = imax then [row] else row :: (readKoppenCol (i+1) imax jmax)
@@ -41,4 +40,3 @@ let readKoppen imax jmax =
     |> List.map (fun x -> x |> List.map (fun (y: Koppen.Zone Option) -> if y.IsNone then "NA\t" else y |> (Option.get >> string >> fun z -> String.concat "" [z; "\t"])))
     |> List.map (fun x -> x |> List.fold (fun acc elem -> String.concat "" [acc; elem]) "" )
     |> fun x -> File.WriteAllLines (@"/local/home/landproj/CMIP6_historical/koppen.out", x)
-    
