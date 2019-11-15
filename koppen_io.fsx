601cb4fbbb3db0149ea67306c527e7602b433b31
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2019-11-15 10:43:13 -0500

    Add main function timer

--- koppen_io.fsx
+++ koppen_io.fsx
@@ -7,21 +7,21 @@ open System.IO
 open FSharp.Data
 
 
-type KoppenFile = CsvProvider<"/local/home/landproj/CMIP6_historical/tmp/100_135.csv",
+type KoppenFile = CsvProvider<"/local/home/landproj/CMIP6_historical/tmp2/100_135.csv",
                               HasHeaders = true,
                               Schema = "float, string, float, float">
-let file = KoppenFile.Load("/local/home/landproj/CMIP6_historical/tmp/100_135.csv")
+let file = KoppenFile.Load("/local/home/landproj/CMIP6_historical/tmp2/100_135.csv")
 
 let imax = 360
 let jmax = 180
 
-let readKoppen imax jmax =
+let readKoppen imax jmax (timer: System.Diagnostics.Stopwatch) =
 
-    let rec readKoppenCol i imax jmax =
-        let rec readKoppenRow i j jmax =
-            let fname = String.concat "" ["/local/home/landproj/CMIP6_historical/tmp/"; string i; "_"; string j; ".csv"]
+    let rec readKoppenCol i imax jmax (timer: System.Diagnostics.Stopwatch) =
+        let rec readKoppenRow i j jmax (timer: System.Diagnostics.Stopwatch) =
+            let fname = String.concat "" ["/local/home/landproj/CMIP6_historical/tmp2/"; string i; "_"; string j; ".csv"]
             if not(File.Exists fname) then
-                if j = jmax then [None] else None :: (readKoppenRow i (j+1) jmax)
+                if j = jmax then [None] else None :: (readKoppenRow i (j+1) jmax timer)
             else
                 let file = KoppenFile.Load(fname)
 
@@ -30,13 +30,20 @@ let readKoppen imax jmax =
                 let temperature =   file.Rows |> Seq.map (fun x -> x.Temperature)   |> Seq.map (float >> (*) 1.0<C>) |> Seq.toArray
                 let precipitation = file.Rows |> Seq.map (fun x -> x.Precipitation) |> Seq.map (float >> (*) 1.0<mm>) |> Seq.toArray
 
-                Koppen.Climate (daysInFebruary, hemisphere, temperature, precipitation)
-                |> Koppen.Zones
-                |> fun x -> if j = jmax then [Some x] else Some x :: (readKoppenRow i (j+1) jmax)
-        let row = readKoppenRow i 1 jmax
-        if i = imax then [row] else row :: (readKoppenCol (i+1) imax jmax)
+                let clim = Koppen.Climate (daysInFebruary, hemisphere, temperature, precipitation)
+                timer.Start()
+                let zone = Koppen.Zones clim
+                timer.Stop()
+                if j = jmax then [Some zone] else Some zone :: (readKoppenRow i (j+1) jmax timer)
+        let row = readKoppenRow i 1 jmax timer
+        if i = imax then [row] else row :: (readKoppenCol (i+1) imax jmax timer)
 
-    readKoppenCol 1 imax jmax
+    readKoppenCol 1 imax jmax timer
     |> List.map (fun x -> x |> List.map (fun (y: Koppen.Zone Option) -> if y.IsNone then "NA\t" else y |> (Option.get >> string >> fun z -> String.concat "" [z; "\t"])))
     |> List.map (fun x -> x |> List.fold (fun acc elem -> String.concat "" [acc; elem]) "" )
-    |> fun x -> File.WriteAllLines (@"/local/home/landproj/CMIP6_historical/koppen.out", x)
+    |> fun x -> File.WriteAllLines (@"/local/home/landproj/CMIP6_historical/koppen_empirical.out", x)
+
+
+let koppenTimer = System.Diagnostics.Stopwatch.StartNew()
+readKoppen imax jmax koppenTimer
+printfn "%i\n" koppenTimer.ElapsedMilliseconds
