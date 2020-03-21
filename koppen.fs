12b33861b712f075ba085c7ee4dced63e0dc7773
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2020-03-21 19:10:27 -0400

    dictonary speedups; map difference sketch
    Distance calculation now working as initially planned, but shows strong regional biases due to the empirical weighting scheme.

--- koppen.fs
+++ koppen.fs
@@ -1,6 +1,8 @@
 
 namespace Koppen
 
+open System.Threading.Tasks
+
 [<Measure>] type C
 [<Measure>] type mm
 [<Measure>] type m
@@ -140,3 +142,273 @@ module Koppen =
             | Temperate   -> clim |> MatchTemperate
             | Continental -> clim |> MatchContinental
             | Polar       -> clim |> MatchPolar
+
+
+    let private allA = [Af; Am; Aw]
+
+    let private allC =
+        [
+            Csa; Csb; Csc;
+            Cfa; Cfb; Cfc;
+            Cwa; Cwb; Cwc
+        ]
+
+    let private allD =
+        [
+            Dsa; Dsb; Dsc; Dsd;
+            Dfa; Dfb; Dfc; Dfd;
+            Dwa; Dwb; Dwc; Dwd;
+        ]
+
+    let ZoneList = allA @ [BWh; BWk; BSh; BSk] @ allC @ allD @ [ET; EF]
+
+    /// List of all valid Koppen zone transitions, given as a zone and its neighbors.
+    let TransitionList =
+        [
+            Af, [Am; Aw; BSh] @ allC;
+            Am, [Af; Aw; BSh] @ allC;
+            Aw, [Af; Am; BSh] @ allC;
+
+            BWh, [BSh; BWk];
+            BWk, [BSk; BWh];
+            BSh, allA @ [BWh; BSk] @ allC;
+            BSk, [BSh; BWk] @ allD @ [ET];
+
+            Csa, allA @ [BSh; Dsa; Csb; Cfa];
+            Csb, allA @ [BSh; Dsb; Csa; Csc; Cfb];
+            Csc, allA @ [BSh; Dsc; Csb; Cfc]            @ [ET];
+            Cfa, allA @ [BSh; Dfa; Csa; Cwa; Cfb];
+            Cfb, allA @ [BSh; Dfb; Csb; Cwb; Cfa; Cfc];
+            Cfc, allA @ [BSh; Dfc; Csc; Cwc; Cfb]       @ [ET];
+            Cwa, allA @ [BSh; Dwa; Cwb; Cfa];
+            Cwb, allA @ [BSh; Dwb; Cfb; Cwa; Cwc];
+            Cwc, allA @ [BSh; Dwc; Cfc; Cwb]            @ [ET];
+
+            Dsa, [BSk; Csa; Dsb; Dfa]
+            Dsb, [BSk; Csb; Dsa; Dsc; Dfb];
+            Dsc, [BSk; Csc; Dsb; Dsd; Dfc]              @ [ET];
+            Dsd, [BSk;      Dsc; Dfd]                   @ [ET];
+            Dfa, [BSk; Cfa; Dsa; Dwa; Dfb];
+            Dfb, [BSk; Cfb; Dsb; Dwb; Dfa; Dfc];
+            Dfc, [BSk; Cfc; Dsc; Dwc; Dfb; Dfd]         @ [ET];
+            Dfd, [BSk;      Dsd; Dwd; Dfc]              @ [ET];
+            Dwa, [BSk; Cwa; Dwb; Dfa];
+            Dwb, [BSk; Cwb; Dfb; Dwa; Dwc];
+            Dwc, [BSk; Cwc; Dfc; Dwb; Dwd]              @ [ET];
+            Dwd, [BSk;      Dfd; Dwc]                   @ [ET];
+
+            ET, [Csc; Cfc; Cwc; Dsc; Dsd; Dfc; Dfd; Dwc; Dwd; EF];
+            EF, [ET]
+        ]
+
+    let Transitions (zone: Zone) =
+        TransitionList
+        |> List.find (fun x -> fst x = zone)
+        |> snd
+
+
+    type private Halo(north: 'a, east: 'a, south: 'a, west: 'a) =
+        member this.North = north
+        member this.East = east
+        member this.South = south
+        member this.West = west
+
+    /// Halo for a regular grid wrapped horizontally
+    let private halo i j (grid: 'a list list) =
+
+        let east =
+            if i = grid.Length - 1 then 0 else i + 1
+            |> fun x -> grid.[x].[j]
+        let west =
+            if i = 0 then grid.Length - 1 else i - 1
+            |> fun x -> grid.[x].[j]
+        let north = if j = grid.[i].Length - 1 then None else grid.[i].[j+1]
+        let south = if j = 0 then None else grid.[i].[j-1]
+
+        Halo(north, east, south, west)
+
+    let private countTransitions (grid: 'a list list) =
+        let transitions =
+            List.init grid.Length (fun i ->
+                List.init grid.[i].Length (fun j ->
+                    match grid.[i].[j] with
+                    | None   -> []
+                    | Some k ->
+                        grid
+                        |> halo i j
+                        |> fun x -> [x.North; x.East; x.South; x.West]
+                        |> List.filter (fun x -> x.IsSome)
+                        |> List.map (Option.get >> (fun x -> k, x))
+                        |> List.filter (fun x -> fst x <> snd x)
+                )
+            )
+            |> List.reduce (@)
+            |> List.reduce (@)
+
+        // If there is a valid transition, return the transition indices
+        let transitionIndex zone1 zone2 =
+            let first =
+                TransitionList
+                |> List.findIndex (fun x -> fst x = zone1)
+            let second =
+                TransitionList.[first]
+                |> snd
+                |> List.tryFindIndex (fun x -> x = zone2)
+            match second with
+            | None -> None
+            | Some j -> Some (first, j)
+
+        // Initialize transition buckets to zero
+        let buckets =
+            Array.init TransitionList.Length (fun i ->
+                Array.init (TransitionList.[i] |> snd |> List.length) (fun j ->
+                    0
+                )
+            )
+
+        let coords =
+            transitions
+            |> List.map (fun x -> transitionIndex (fst x) (snd x))
+            |> List.choose id
+
+        for i, j in coords do
+            buckets.[i].[j] <- buckets.[i].[j] + 1
+        buckets
+
+    let private graphWeights (cost: int -> float) (grid: 'a list list) =
+        countTransitions grid
+        |> Array.toList
+        |> List.map (fun x ->
+            x
+            |> Array.toList
+            |> List.map cost
+        )
+
+    let Graph (cost: int -> float) (grid: 'a list list) =
+        (TransitionList |> List.map snd, graphWeights cost grid)
+        ||> List.map2 List.zip
+        |> fun x -> (TransitionList |> List.map fst, x)
+        ||> List.zip
+
+    let private graphKeys (cost: int -> float) (grid: 'a list list) =
+        Graph cost grid
+        |> List.map (fun x ->
+            snd x
+            |> List.map (fun y ->
+                fst x, fst y
+            )
+        )
+
+    let GraphAsMap (cost: int -> float) (grid: 'a list list) =
+        (graphKeys cost grid, graphWeights cost grid)
+        ||> List.map2 List.zip
+        |> List.reduce (@)
+        |> Map.ofList
+
+    //Dijkstra's algorithm: Nigel Galloway, August 5th., 2018
+    // from https://rosettacode.org/wiki/Dijkstra%27s_algorithm#F.23
+    [<CustomEquality;CustomComparison>]
+    type private Dijkstra<'N, 'G when 'G:comparison>
+        = {toN: 'N; cost: Option<'G>; fromN: 'N}
+            override g.Equals n =
+                match n with
+                | :? Dijkstra<'N,'G> as n -> n.cost = g.cost
+                | _ -> false
+            override g.GetHashCode() = hash g.cost
+            interface System.IComparable with
+                member n.CompareTo g =
+                    match g with
+                    | :? Dijkstra<'N,'G> as n when n.cost = None -> (-1)
+                    | :? Dijkstra<'N,'G>      when n.cost = None -> 1
+                    | :? Dijkstra<'N,'G> as g                    -> compare n.cost g.cost
+                    | _-> invalidArg "n" "expecting type Dijkstra<'N,'G>"
+
+    // from https://rosettacode.org/wiki/Dijkstra%27s_algorithm#F.23
+    let inline private Dijkstra N G y =
+        let rec fN l f =
+            if List.isEmpty l then f else
+                let n = List.min l
+                if n.cost=None then f else
+                fN (l |> List.choose (fun n' ->
+                    if n'.toN=n.toN then None else
+                    match n.cost, n'.cost, Map.tryFind (n.toN,n'.toN) G with
+                    | Some g, None,    Some wg                  -> Some {toN = n'.toN; cost = Some(g+wg); fromN = n.toN}
+                    | Some g, Some g', Some wg when g + wg < g' -> Some {toN = n'.toN; cost = Some(g+wg); fromN = n.toN}
+                    | _                                         -> Some n'))
+                    ((n.fromN, n.toN) :: f)
+
+        let r = fN (N |> List.map (fun n -> {toN = n; cost = (Map.tryFind (y,n) G); fromN = y})) []
+        (fun n ->
+            let rec fN z l =
+                match List.tryFind (fun (_,g) -> g = z) r with
+                | Some (n', g') when y = n'-> Some (n' :: g' :: l)
+                | Some (n', g')            -> fN n' (g' :: l)
+                | _                        -> None
+            fN n [])
+
+    let private dictionary func =
+        //let keys = graphKeys cost grid |> List.reduce (@)
+        let keys =
+            List.init ZoneList.Length (fun i ->
+                List.init ZoneList.Length (fun j ->
+                    printf "%i    %i\n" i j
+                    (ZoneList.[i], ZoneList.[j])))
+            |> List.reduce (@)
+        printfn "keys length: %i\n" keys.Length
+
+        let timer = System.Diagnostics.Stopwatch.StartNew()
+        timer.Start()
+        let vals =
+            keys
+            |> List.map (fun x -> func (fst x) (snd x))
+        timer.Stop()
+        printfn "%i s\n" timer.ElapsedMilliseconds
+
+        printfn "%s\n" "here2"
+        (keys, vals)
+        ||> List.zip
+
+
+    let Paths (cost: int -> float) (grid: 'a list list) =
+        Dijkstra ZoneList (GraphAsMap cost grid)
+
+    let PathDictionary (cost: int -> float) (grid: 'a list list) =
+        Paths cost grid |> dictionary
+
+
+
+    let Distance (cost: int -> float) (grid: 'a list list) (zone1: Zone) (zone2: Zone)
+        (pathDictionary: ((Zone * Zone) * Zone list Option) list) =
+        let map = GraphAsMap cost grid
+
+        pathDictionary
+        |> List.find (fun ((x, y), z) -> (x, y) = (zone1, zone2))
+        |> snd
+        |> function
+            | None -> infinity
+            | Some path ->
+                List.init (path.Length-1) (fun i ->
+                    map |> Map.find (path.[i], path.[i+1])
+                )
+                |> List.reduce (+)
+
+    let DistanceDictionary (cost: int -> float) (grid: 'a list list) =
+        let pathDictionary = PathDictionary cost grid
+        (fun a b -> Distance cost grid a b pathDictionary)
+        |> dictionary
+
+    let Difference (cost: int -> float) (grid1: Zone Option list list) (grid2: Zone Option list list) =
+        let distanceDictionary = DistanceDictionary cost grid1
+        List.init grid1.Length (fun i ->
+            List.init grid2.Length (fun j ->
+                printf "%i    %i\n" i j
+                match grid1.[i].[j], grid2.[i].[j] with
+                | None, None -> None
+                | Some zone1, Some zone2 ->
+                    distanceDictionary
+                    |> List.find (fun ((x, y), z) -> (x, y) = (zone1, zone2))
+                    |> snd
+                    |> Some
+                | _, _ -> failwith "Grids don't match"
+            )
+        )
